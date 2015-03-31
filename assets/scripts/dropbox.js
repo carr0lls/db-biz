e33fb79f2fe725b550f584cb2c62fd180042c2fc
Carroll Yu <carrollcyu@gmail.com>
2015-03-31 16:04:03 -0700

    Import existing project to github

---
+++ assets/scripts/dropbox.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+	// $('.header-bg-image').animate({opacity: '1', height: '500px', marginTop: -500}, 3000);
+	$('#header .header-bg-image').animate({opacity: '1', backgroundPositionY: -98}, 3000);
+
+});
